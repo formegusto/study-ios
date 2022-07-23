36d7a2019ec4ca5b533068fdaf85197251792e29
formegusto <formegusto@gmail.com>
2022-07-23 21:00:57 +0900

    Gesture Event

--- 05#GestureEvent/05#GestureEvent/Controller/MainViewController.swift
+++ 05#GestureEvent/05#GestureEvent/Controller/MainViewController.swift
@@ -1,11 +1,22 @@
 import UIKit
 
+enum DragType {
+    case X
+    case Y
+    case none
+}
 class MainViewController: UIViewController {
 
+    var dragType: DragType = .none {
+        didSet {
+            self.myView.dragType = dragType
+        }
+    }
+    let myView = DraggableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myView = DraggableView()
         myView.center = self.view.center
         myView.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
         myView.backgroundColor = .red
@@ -13,6 +24,18 @@ class MainViewController: UIViewController {
         self.view.addSubview(myView)
     }
 
-
+    @IBAction func setXY(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.dragType = .X
+        case 1:
+            self.dragType = .Y
+        case 2:
+            self.dragType = .none
+        default:
+            break
+        }
+    }
+    
 }
 
--- 05#GestureEvent/05#GestureEvent/View/DraggableView.swift
+++ 05#GestureEvent/05#GestureEvent/View/DraggableView.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class DraggableView: UIView {
+    var dragType: DragType = .none
+    
     init() {
         super.init(frame: CGRect.zero)
         
@@ -18,20 +20,38 @@ class DraggableView: UIView {
                 let delta = pan.translation(in: self.superview)
                 
                 var viewPosition = self.center
-                viewPosition.x += delta.x
-                viewPosition.y += delta.y
-            
+                if dragType == .X {
+                    viewPosition.x += delta.x
+                } else if dragType == .Y {
+                    viewPosition.y += delta.y
+                } else {
+                    viewPosition.x += delta.x
+                    viewPosition.y += delta.y
+                }
+                
                 self.center = viewPosition
                 pan.setTranslation(CGPoint.zero, in: self.superview)
             case .cancelled, .ended:
-                print("누른 상태에서 그냥 멀리 이동하는 경우")
-                print("그리고 끝났을 때")
-                default:
-                    break
+//                print("누른 상태에서 그냥 멀리 이동하는 경우")
+//                print("그리고 끝났을 때")
+                if self.frame.minX < 0 {
+                    self.frame.origin.x = 0
+                }
+                if let hasSuper = self.superview {
+                    if self.frame.maxX > hasSuper.frame.maxX {
+                        self.frame.origin.x = hasSuper.frame.maxX - self.bounds.width
+                    }
+                }
+            default:
+                break
         }
     }
     
     required init?(coder: NSCoder) {
+//        super.init(coder: coder)
+//        
+//        let pan = UIPanGestureRecognizer(target: self, action: #selector(dragging))
+//        self.addGestureRecognizer(pan)
         fatalError("init(coder:) has not been implemented")
     }
 }
